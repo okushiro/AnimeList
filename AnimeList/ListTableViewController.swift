f91ed810e9d84d736e4ce4a6b49440b908af0344
奥城健太郎 <ken09460308@gmail.com>
2019-01-29 19:21:22 +0900

    APIの情報をテーブルに表示完了

---
+++ AnimeList/ListTableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ListTableViewController.swift
+//  AnimeList
+//
+//  Created by 奥城健太郎 on 2019/01/27.
+//  Copyright © 2019 奥城健太郎. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ListTableViewController: UITableViewController {
+    
+    let userDefaults = UserDefaults.standard
+    
+    let setYear : [String] = ["2014年", "2015年", "2016年", "2017年", "2018年", "2019年"]
+    let setSeason : [String] = ["冬アニメ", "春アニメ", "夏アニメ", "秋アニメ"]
+    
+    var animeList:[[String:String]] = [[:]]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //選択したクールを取得
+        guard let yearIndex:Int = userDefaults.object(forKey: "yearKey") as? Int else{
+            return
+        }
+        guard let seasonIndex:Int = userDefaults.object(forKey: "seasonKey") as? Int else{
+            return
+        }
+        print(setYear[yearIndex])
+        print(setSeason[seasonIndex])
+        
+        animeList = (userDefaults.object(forKey: "animeList") as? [[String: String]])!
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return animeList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the cell...
+        
+        cell.textLabel?.text = animeList[indexPath.row]["title"]
+
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AnimeList/ViewController.swift
+++ AnimeList/ViewController.swift
@@ -7,14 +7,123 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // UIPickerViewの列の数
+    func numberOfComponents(in yearPicker: UIPickerView) -> Int {
+        return 2
+    }
+    
+    // UIPickerViewの行数、リストの数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        if component == 0{
+            return setYear.count
+        }else{
+            return setSeason.count
+        }
+    }
+    
+    // UIPickerViewの最初の表示
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        if component == 0{
+            return setYear[row]
+        }else{
+            return setSeason[row]
+        }
+    }
+    
+    
+    @IBOutlet weak var yearPicker: UIPickerView!
+    
+    let userDefaults = UserDefaults.standard
+    
+    //PickerViewの内容
+    let setYear : [String] = ["2014年", "2015年", "2016年", "2017年", "2018年", "2019年"]
+    let setSeason : [String] = ["冬アニメ", "春アニメ", "夏アニメ", "秋アニメ"]
+    
+//    let yearKey = "year_value"
+//    let seasonKey = "season_value"
+    
+    var yearRow:Int = 0
+    var seasonRow:Int = 0
+    
+    var animeList : [(
+        title:String, twitter_hash_tag:String, public_url:String, twitter_account:String
+        )] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        yearPicker.delegate = self
+        yearPicker.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func didTouchSearchBtn(_ sender: Any) {
+        // 1列目の選択されているrowの取得
+        yearRow = yearPicker.selectedRow(inComponent: 0)
+        userDefaults.set(yearRow, forKey: "yearKey")
+        // 2列目の選択されているrowの取得
+        seasonRow = yearPicker.selectedRow(inComponent: 1)
+        userDefaults.set(seasonRow, forKey: "seasonKey")
+        
+        //URL
+        let url:String = "https://api.moemoe.tokyo/anime/v1/master/\(yearRow+2014)/\(seasonRow+1)"
+        
+        //APIの取得
+        Alamofire.request(
+            url,
+            method: .get,
+            parameters: [:],
+            encoding: URLEncoding.default,
+            headers: nil
+            )
+            .responseJSON{(response: DataResponse<Any>) in
+                
+                let json = JSON(response.result.value as Any)
+                
+                self.animeList = []
+                for item in 0 ..< json.count {
+                    if let title = json[item]["title"].string,
+                        let twitter_hash_tag = json[item]["twitter_hash_tag"].string,
+                        let public_url = json[item]["public_url"].string,
+                        let twitter_account = json[item]["twitter_account"].string {
+                        let animeInfo = (title, twitter_hash_tag, public_url, twitter_account)
+                        self.animeList.append(animeInfo)
+                    }
+                }
+                
+                //辞書型に変更して保存
+                let saveData: [[String: Any]] = self.animeList.map {[
+                    "title": $0.title,
+                    "twitter_hash_tag": $0.twitter_hash_tag,
+                    "public_url": $0.public_url,
+                    "twitter_account": $0.twitter_account
+                    ]}
+                self.userDefaults.set(saveData, forKey: "animeList")
+                self.performSegue(withIdentifier: "toList", sender: nil)
+        }
+        
+        //リスト表示ページへ
+//        if animeList.isEmpty {return}
+//        self.userDefaults.set(self.animeList, forKey: "animeList")
+//        performSegue(withIdentifier: "toList", sender: nil)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let next = segue.destination as? ListTableViewController
+//        let _ = next?.view
+//        next?.yearRow = yearRow
+//        next?.seasonRow = seasonRow
+//    }
+    
 }
 
